0bf7170ce4bd3e9dc9309c1849c29180546bebcd
Jesús Lázaro <jlazaro135@gmail.com>
2019-10-14 00:15:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+mood = input ("How do you feel, mate?: ")
+
+
+if mood == "happy":
+    print ("It is great to see you happy!")
+
+elif mood == "nervous":
+    print ("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print ("My friend tell me, I am psychologist")
+
+elif mood == "excited":
+    print ("Take easy, we've just started learning Python")
+
+elif mood == "relaxed":
+    print ("Ok mate, looks easier than HTML and CSS but keep paying attention to MCarmen")
+
+else:
+    print ("I've never heard that mood, are you Eskimo?")
+
